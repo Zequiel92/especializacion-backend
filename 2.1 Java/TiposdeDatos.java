230f5f78e3f30eb37e177c5d827872c4b62dc131
AlanAlvarez <fantamigosmg@gmail.com>
2024-10-31 15:15:31 -0300

    codigo java 3

---
+++ 2.1 Java/TiposdeDatos.java
@@ -0,0 +1,52 @@
+public class TiposdeDatos {
+	public static void main(String[] args) {
+		
+		// Enteros
+
+		byte num1 = 20;
+
+		short num2 = 30;
+
+		int num3;
+		num3 = 50;
+
+		long num4 = 60;
+
+		System.out.println(num4);
+
+		// Flotantes
+
+		float numeroFlotante = 21.10F;
+
+		double numerDoble = 3.1416D;
+
+		System.out.println("Flotantes "+numeroFlotante);
+		System.out.println("Double "+numerDoble);
+
+
+		// Character
+
+		char minusculaA = 'a';
+		char minusculaO = 'o';
+
+		System.out.println("Letra "+minusculaA+", letra "+minusculaO);
+
+
+		// Booleanos
+
+		boolean verdadero = true;
+		boolean falso = false;
+
+		System.out.println(verdadero);
+
+
+		// String o Cadena
+
+		String persona1 = "Monica";
+
+		var persona2 = "Ezequiel"; // También se puede usar var para declarar variables
+
+		System.out.println("Hola mi nombre es "+persona2+" y amigo de "+persona1);
+
+	}
+}
